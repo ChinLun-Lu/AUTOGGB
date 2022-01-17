00c2c22e3311e70fcaa05034b67b53b65f145678
ChinLun-Lu <59011284+ChinLun-Lu@users.noreply.github.com>
2022-01-17 13:49:52 +0800

    Add files via upload
    Add on 2022/01/17
    - with test data
    - can show confusion matrix

--- main.py
+++ main.py
@@ -2,35 +2,40 @@ import pandas as pd
 import sentencetovector
 import kNN
 import matplotlib.pyplot as plt
+import seaborn as sn
 
 ### preprocessing ###
 # read data
-df = pd.read_excel("Autoggb_trainingdata.xlsx")
+df1 = pd.read_csv("Autoggb_trainingdata.csv")
 
 #擷取Sentence和Type欄位，並且轉換成list形式
-sentence_list = df['Sentence'].tolist()
-type_list = df['Type'].tolist()
-#M = len(sentence_list)
+sentence_list = df1['Sentence'].tolist()
+type_list = df1['Type'].tolist()
 
 # 關鍵字列表
 feature_list = ['圓', '線段', '直線', '中垂線', '中點', '拋物線']
 N = len(feature_list)
 # 動詞、其他數學名詞列表
 verb_list = ['作', '畫', '求']
-otherwise_list = ['圓心', '半徑', '準線', '焦點', '交點', '原點', '斜率']
+otherwise_list = ['圓心', '半徑', '點', '直徑', '準線', '焦點', '交點', 
+                  '原點', '斜率', '三角形', '正方形', '橢圓', '長方形',
+                  'x軸', 'y軸', '外接圓']
 # 將sentence_list 轉換成向量
 vector_list = []
 for item1 in sentence_list:
     vector_list.append(sentencetovector.get_FEATURE_FI_2(item1, feature_list, verb_list, otherwise_list))
 
-### training ###
-# 欲分類資料
-stest_list = ['畫一個1公分的線段', '畫一個半徑是r的圓', '作一條斜率為m的直線', 'test']
-stype_list = ['Segment', 'Circle', 'Line', 'Otherwise']
+### predict ###
+# 讀取欲分類資料
+df2 = pd.read_csv('Autoggb_testdata.csv')
+stest_list = df2['Sentence'].tolist()
+stype_list = df2['Type'].tolist()
 # 預測list
 pred_list = []
 # value of 'k'
-k = 5
+k = 7
+
+# predict model
 for i in range(len(stype_list)):
     # 用feature_list先做篩選
     if any(ext in stest_list[i] for ext in feature_list):
@@ -59,12 +64,14 @@ print(pred_list)
 print(stype_list)
 
 # confusion matrix 分析
-# 實際情況
+# 實際
 y_actu = pd.Series(stype_list, name='Actual')
-# 預測情況
+# 預測
 y_pred = pd.Series(pred_list, name='Predicted')
 # confusion matrix
 df_confusion = pd.crosstab(y_actu, y_pred)
 print(df_confusion)
+# draw confusion matrix
+sn.heatmap(df_confusion, annot=True)
+plt.show()
 
-# 訓練k
